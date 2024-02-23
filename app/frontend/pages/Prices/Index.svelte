61a002649f47f9257dd3cd9534152b1b55c3af40
Peter Bell <peter.bell215@gmail.com>
2024-02-23 22:25:53 +0000

    Improvement to rendering the Prices page.

--- app/frontend/pages/Prices/Index.svelte
+++ app/frontend/pages/Prices/Index.svelte
@@ -6,25 +6,32 @@
     export let prices;
 </script>
 
-<h1>History of charges for {client.name}</h1>
+<div class="mx-auto col-4 py-4">
+    <div class="card mb-4">
+        <div class="card-header py-3">
+            <h5 class="mb-0">History of charges for {client.name}</h5>
+        </div>
+        <div class="card-body">
+            <Link href="/clients/{client.id}" class="btn btn-primary">Back</Link>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>From</th>
+                        <th>To</th>
+                        <th>Hourly Charge Rate</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {#each prices as price}
+                        <tr>
+                            <td>{format_date(price.from)}</td>
+                            <td>{format_date(price.to)}</td>
+                            <td>{Dinero(price.hourly_charge_rate_as_dinero).toFormat('$0,0')}</td>
+                        </tr>
+                    {/each}
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
 
-<table class="table table-striped">
-    <thead>
-        <tr>
-            <th>From</th>
-            <th>To</th>
-            <th>Hourly Charge Rate</th>
-        </tr>
-    </thead>
-    <tbody>
-        {#each prices as price}
-            <tr>
-                <td>{format_date(price.from)}</td>
-                <td>{format_date(price.to)}</td>
-                <td>{Dinero(price.hourly_charge_rate_as_dinero).toFormat('$0,0')}</td>
-            </tr>
-        {/each}
-    </tbody>
-</table>
-
-<Link href="/clients/{client.id}" class="btn btn-primary">OK</Link>
