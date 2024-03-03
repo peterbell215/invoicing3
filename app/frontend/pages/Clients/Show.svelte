fa2c785bfce6fdcc86ebc01979631ebb2c289743
Peter Bell <peter.bell215@gmail.com>
2024-03-03 06:14:01 +0000

    On 'clients/:id/show' page make all fields readonly

--- app/frontend/pages/Clients/Show.svelte
+++ app/frontend/pages/Clients/Show.svelte
@@ -10,16 +10,17 @@
     <svelte:fragment slot="navigation-elements">
         <Link href="/clients/{client.id}/edit" class="btn btn-primary">Edit</Link>
         <Link href="/clients/{client.id}/fees" class="btn btn-primary">Fee History</Link>
+        <Link href="/client_sessions" data={ {client_id: client.id} } class="btn btn-primary">Client Sessions</Link>
     </svelte:fragment>
 
     <fieldset slot="form-elements" let:form class="form-group border p-3">
         <legend class="w-auto px-2">Fees</legend>
         <div class="row">
             <div class="col">
-                <FormInput {form} field="current_rate" label_name="Current Hourly Charge"/>
+                <FormInput {form} field="current_rate" label_name="Current Hourly Charge" readonly="true"/>
             </div>
             <div class="col">
-                <FormInput {form} field="current_rate_since" label_name="Since"/>
+                <FormInput {form} field="current_rate_since" label_name="Since" readonly="true"/>
             </div>
         </div>
     </fieldset>
