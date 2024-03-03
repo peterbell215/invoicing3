cc14e538611ddbe9c7d67a51d1fc7e6efa62ba72
Peter Bell <peter.bell215@gmail.com>
2024-03-03 06:11:51 +0000

    Add a "client_sessions/show" path to app

---
+++ app/frontend/pages/ClientSessions/Show.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { useForm } from '@inertiajs/svelte'
+    import ClientSessionForm from "~/pages/ClientSessions/ClientSessionForm.svelte";
+    import FormInput from "@/components/FormInput.svelte";
+
+    export let clients;
+    export let client_session;
+</script>
+
+<ClientSessionForm {client_session} {clients}>
+    <fieldset slot="form-elements" let:form class="form-group border p-3">
+        <legend class="w-auto px-2">Fees</legend>
+        <div class="row">
+            <div class="col">
+                <FormInput {form} field="current_rate" label_name="Current Hourly Charge" />
+            </div>
+        </div>
+    </fieldset>
+</ClientSessionForm>
